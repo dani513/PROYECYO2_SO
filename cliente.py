7b94c0ed8273a5fe5e6d715d5975832d0e17ff88
DuniaMarquina <60081143+DuniaMarquina@users.noreply.github.com>
2023-11-08 16:09:19 -0400

    Documentacion

--- cliente.py
+++ cliente.py
@@ -1,5 +1,4 @@
 from socket import socket, AF_INET, SOCK_STREAM
-import pickle
 import os
 from datetime import datetime
 import json
@@ -9,8 +8,16 @@ from io import StringIO
 
 
 def main():
-    client_socket = socket(AF_INET, SOCK_STREAM)
-    client_socket.connect(('localhost', 12345))
+    """
+    Función principal que maneja la interacción con el usuario y la comunicación con el servidor.
+
+    Conecta el cliente al servidor, muestra un menú al usuario y procesa su selección. 
+    Las opciones incluyen agregar una canción,mostrar la lista de reproducción y salir del programa. 
+
+    La comunicación con el servidor se realiza a través de un socket.
+    """
+    client_socket = socket(AF_INET, SOCK_STREAM)  # Creación de un objeto socket
+    client_socket.connect(('localhost', 12345))  # conexión al servidor en localhost en el puerto 12345
     
     while True:
         os.system('clear')
@@ -22,28 +29,28 @@ def main():
         opcion = input("Seleccione una opción: ")
         
         if opcion == "1":
+            # Se solicita al usuario que introduzca los atributos de la canción
             titulo = input("Titulo de la canción: ")
             interprete = input("Interprete: ")
             album = input("Album: ")
-            #fecha = input("Fecha de agregación: ")
             usuario = input("Usuario que agrego: ")
             duracion = input("Duracion: ")
             
+            #se crea un diccionario con los atributos de la canción
             cancion = {"titulo": titulo, "interprete": interprete, "album": album, "fecha": datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 
                        "usuario":usuario, "duracion": duracion}
-            
+            #Se crea una solicitud con la opción y los atributos de la canción
             solicitud = f"1 {json.dumps(cancion)}"
-            client_socket.send(solicitud.encode())
-            respuesta = client_socket.recv(1024).decode()
+            client_socket.send(solicitud.encode())  #se envía la solicitud al servidor
+            respuesta = client_socket.recv(1024).decode() 
             print(respuesta)
         
         elif opcion == "2":
             
-            solicitud = "2"
-            client_socket.send(solicitud.encode())
-            data_string = client_socket.recv(1024).decode()
+            solicitud = "2"  #se crea una solicitud con la opción
+            client_socket.send(solicitud.encode())  #se envía la solicitud al servidor
+            data_string = client_socket.recv(1024).decode()  #se recibe la respuesta del servidor
             print("Lista de Reproducción:")
-            print(type(data_string))
             print(data_string)
             df = pd.read_csv(StringIO(data_string))
             print("\n2 ultimas canciones agregadas recientemente:")
@@ -51,11 +58,14 @@ def main():
             input("Presiona Enter para continuar...")
             
         elif opcion == "3":
-            break
+            break  
         
         else:
             print("Opción no válida. Por favor, elija una opción del menú.")
             input("Presiona Enter para continuar...")
 
 if __name__ == "__main__":
+    """
+    Verifica si el script se está ejecutando como programa principal. Si es así, llama a la función main().
+    """
     main()
--- servidor.py
+++ servidor.py
@@ -1,66 +1,67 @@
-import os
 import pandas as pd
 import time
 import csv
 import threading
 from socket import socket, AF_INET, SOCK_STREAM
-import pickle
 import json
 
 #clase para representar un reproductor de musica con semáforos
 class ReproductorMusica:
+    """
+    Clase para representar un reproductor de música con semáforos.
+
+    Esta clase administra una lista de reproducción de canciones y proporciona métodos para agregar canciones, guardar y modificar la lista de reproducción.
+    También, tiene un semáforo para garantizar la exclusión mutua al agregar anciones.
+    """
     def __init__(self):
+        """
+        Inicializa el reproductor de música.
+        Crea un DataFrame vacío para la lista de reproducción y establece el nombre del archivo para guardar y cargar la lista de reproducción.
+        """
         self.playlist = pd.DataFrame(columns=["Titulo", "Interprete", "Album", "Fecha", "Usuario", "Duracion"])
         self.nombre_archivo = "playlist.csv"
-        self.bloqueo = "bloqueo.txt"
         self.semaforo = threading.Semaphore()
         
     def cargar_playlist(self):
+        """Carga la lista de reproducción desde un archivo CSV."""
         try:
             self.playlist = pd.read_csv(self.nombre_archivo)
         except FileNotFoundError:
             print("El archivo no existe")
 
     def guardar_playlist(self):
+        """Guarda la lista de reproducción en un archivo CSV."""
         self.playlist.to_csv(self.nombre_archivo, index=False)
         
         print("Agregada exitosamente a la playlist")
 
-    def verificar_bloqueo(self):
-        return os.path.isfile(self.bloqueo)
-
-    def crear_bloqueo(self):
-        with open(self.bloqueo, mode='w', newline='') as archivo_bloqueo:
-            escritor = csv.writer(archivo_bloqueo)
-            escritor.writerow(["En proceso"])
-
-    def eliminar_bloqueo(self):
-        os.remove(self.bloqueo)
 
     def agregar_cancion(self, titulo, interprete, album, fecha, usuario, duracion):
+        """
+        Agrega una canción a la lista de reproducción, asegurándose que no exista una 
+        canción con el mismo título e interprete.
+        """
         self.semaforo.acquire()
         self.cargar_playlist()
 
         if not self.playlist[(self.playlist['Titulo'] == titulo) & (self.playlist['Interprete'] == interprete)].empty:
             print(f"La canción '{titulo}' de '{interprete}' ya está en la playlist.")
             time.sleep(3)
-            #self.eliminar_bloqueo()
+
             self.semaforo.release()
             return
-        
-        #self.crear_bloqueo()        
+             
         nueva_cancion = pd.DataFrame([[titulo, interprete, album, fecha, usuario, duracion]],
                                       columns=["Titulo", "Interprete", "Album", "Fecha", "Usuario", "Duracion"])
         self.playlist = pd.concat([self.playlist, nueva_cancion], ignore_index=True)
-        print("si llega")
         time.sleep(5)
         self.guardar_playlist()
         self.semaforo.release()
 
         time.sleep(2)
-        #self.eliminar_bloqueo()
 
     def mostrar_playlist(self):
+        """Muestra la lista de reproducción."""
         self.cargar_playlist()
         ultimas_canciones = self.playlist.tail(2)
         print("\nLista de Reproducción:") 
@@ -70,18 +71,35 @@ class ReproductorMusica:
         time.sleep(10)
 
     def ver_playlist(self):
+        """Retorna la lista de reproducción."""
         self.cargar_playlist()
         return self.playlist()
         
 #clase para representar el servidor
 class ServidorReproductor:
+    """
+    Clase para representar el servidor del reproductor de música.
+
+    Esta clase gestiona la comunicación entre el cliente y el reproductor de música.
+    """
     def __init__(self, reproductor):
+        """
+        Inicializa el servidor del reproductor de música.
+
+        Establece el reproductor de música que se utilizará.
+        """
         self.reproductor = reproductor
 
     def ejecutar_servidor(self):
-        server_socket = socket(AF_INET, SOCK_STREAM)
-        server_socket.bind(('localhost', 12345))
-        server_socket.listen(5)
+        """
+        Ejecuta el servidor del reproductor de música.
+
+        Crea un socket de servidor y espera conexiones de clientes.
+        Cuando se establece una conexión, crea un nuevo hilo para atender al cliente.
+        """
+        server_socket = socket(AF_INET, SOCK_STREAM)  #creación de un objeto socket
+        server_socket.bind(('localhost', 12345))  #vincula el socket del servidor a la dirección localhost y al puerto 12345
+        server_socket.listen(5)  #permite al servidor escuchar conexiones entrantes
         
         while True:
             print("Esperando conexiones...")
@@ -91,38 +109,47 @@ class ServidorReproductor:
             client_thread.start()
 
     def atender_cliente(self, client_socket):
+        """
+        Maneja la solicitud de un cliente.
+
+        Recibe solicitudes del cliente y las procesa.
+        Las solicitudes pueden ser para agregar una canción a la lista de reproducción o para obtener la lista de reproducción.
+        """
         while True:
             
             solicitud = client_socket.recv(1024).decode()
             if not solicitud: 
                 break
             
-            if solicitud == "2":
+            if solicitud == "2":  #solicitud de mostrar la lista
                 playlist = pd.read_csv("playlist.csv")
                 resp = playlist.to_string(index=False)
-                #playlist_data = self.reproductor.playlist.to_dict(orient='records')
-                #data_string = pickle.dumps(playlist_data)
                 
-                x = playlist.tail(2)
                 client_socket.send(resp.encode())
                 
-            elif solicitud == "3":
+            elif solicitud == "3":  #solicitud de salir
                 break
                       
-            elif solicitud.startswith('1'):
-                print("past")
+            elif solicitud.startswith('1'):  #solicitud para agregar una canción
                 cancion = json.loads(solicitud.split(" ",1)[1])
                 self.reproductor.agregar_cancion(**cancion)
                 client_socket.send("Canción agregada exitosamente".encode())
-        client_socket.close()
+        client_socket.close()  #cierra la conexión con el cliente
                 
 #funcion para ejecutar el servidor
 def ejecutar_servidor():
+    """
+    Función para ejecutar el servidor.
+
+    Crea un reproductor de música y un servidor de reproductor, y luego ejecuta el servidor.
+    """
     reproductor = ReproductorMusica()
     servidor = ServidorReproductor(reproductor)
     servidor.ejecutar_servidor()
 
 if __name__ == "__main__":
-    # Inicia el servidor en un hilo separado
+    """
+    Verifica si el script se está ejecutando como programa principal. Si es así, inicia el servidor en un hilo separado.
+    """
     servidor_thread = threading.Thread(target=ejecutar_servidor)
     servidor_thread.start()
